80b8ca4cf9b6106f8b7e64c2605042d4aea5b4b1
bharat_raithi <124795507+bharatkumarraith@users.noreply.github.com>
2023-11-10 18:59:56 +0530

    Delete Backend-pokemon directory

--- Backend-pokemon/Backend-pokemon/src/main/java/com/example/Backendpokemon/service/Service.java
+++
@@ -1,8 +0,0 @@
-package com.example.Backendpokemon.service;
-
-import com.example.Backendpokemon.Domain.Pokemon;
-@org.springframework.stereotype.Service
-public interface Service {
-
-    Pokemon addPokemon(Pokemon pokemon);
-}
