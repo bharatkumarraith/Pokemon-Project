fc2bf4a634ccc4af0cc10f59e489d0f4cbdfc5f3
bharat_raithi <124795507+bharatkumarraith@users.noreply.github.com>
2023-11-30 19:38:15 +0530

    added file

--- Backend-pokemon/Backend-pokemon/src/main/java/com/example/Backendpokemon/Repository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Backendpokemon;
-
-import com.example.Backendpokemon.Domain.Pokemon;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface Repository extends MongoRepository<Pokemon,String> {
-}
